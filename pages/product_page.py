4a82d97119ff623ced26a396254a5bd51a136d94
bprpforever <ioleynik@zuzex.com>
2020-11-14 20:16:31 +0300

    add pytest fixture and xfail to find the bag on the promo pages. Fix bad expected resuts in test_product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,6 +4,7 @@ from .locators import ProductPageLocators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+import time
 
 
 class ProductPage(BasePage):
@@ -11,6 +12,7 @@ class ProductPage(BasePage):
     def return_product_price(self):
         product_price = self.browser.find_element(By.XPATH, "//p[@class='price_color']")
         return product_price.text
+
 
     print(return_product_price)
 
@@ -29,9 +31,13 @@ class ProductPage(BasePage):
             ec.element_to_be_clickable((By.XPATH, "//button[@value='Add to basket']"))).click()
         WebDriverWait(self.browser, 5).until(ec.alert_is_present())
         self.solve_quiz_and_get_code()
-        product_name2 = self.return_product_name()
-        price2 = self.return_product_price()
-        assert product_name == product_name2
-        print(product_name2)
-        assert price == price2
+        price2 = self.browser.find_elements(By.XPATH, "//div//strong")
+        assert price2[5].text == price, \
+             f"Price is not {price}"
         print(price2)
+        product_name2 = self.browser.find_elements(By.XPATH, "//div//strong")
+        assert product_name2[3].text == product_name, \
+             f"Product name is not {product_name}"
+
+
+
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,14 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
+import pytest
 import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i==10, reason='shit')) for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    browser.delete_all_cookies()
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.guest_can_add_product_to_basket()
