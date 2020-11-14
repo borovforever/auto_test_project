45e7773b72a5a81990ea173227146cdaa32f6a0b
bprpforever <ioleynik@zuzex.com>
2020-11-14 13:35:39 +0300

    add redirects between pages. All tests are running in 2 times instead of 5 times now

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -12,6 +13,8 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,40 +1,25 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
     page.should_be_login_link()
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
-def test_should_be_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
 
-
-def test_should_be_login_form(browser):
+def test_guest_should_see_login_reg_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
     page.should_be_login_form()
-
-
-def test_should_be_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
     page.should_be_register_form()
+
+
